82fbe8b88af10088b5c5ad606afca625d457f201
spsiddarthan <siddharthan@shippable.com>
2015-12-16 15:04:28 +0530

    Added services to u12golpls.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM drydock/u12pls:prod
 
-ADD . /tmp
+ADD . /u12golpls
 
-RUN /tmp/install.sh && rm -rf /tmp
+RUN /u12golpls/install.sh
---
+++ version/1_5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+#set the source path of gvm. $HOME points to '/root'
+[[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm
+
+# Install Go 1.5
+echo "================= Install Go 1.5 ==================="
+sudo gvm install go1.5 --prefer-binary && gvm use go1.1 && go install -a -race std && go get -u github.com/tools/godep
+
